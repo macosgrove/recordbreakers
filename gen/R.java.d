0a58614cb2281143de3368e19633060ab1e0b453
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-10-04 07:50:16 +1000

    Added a utility run task to the build.xml so we can quickly deploy to devices (saves a few steps). Just run 'ant debug install run' and the App will be built, deployed and executed on your device.

---
+++ gen/R.java.d
@@ -0,0 +1,12 @@
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/gen/com/example/RecordBreakers/R.java \
+ : /Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/drawable-hdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/drawable-ldpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/drawable-mdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/drawable-xhdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/layout/main.xml \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/res/values/strings.xml \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/bin/res/drawable-hdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/bin/res/drawable-ldpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/bin/res/drawable-mdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/bin/res/drawable-xhdpi/ic_launcher.png \
+/Users/domlebo70/Documents/workspace/ThoughtWorks/android/recordbreakers/bin/AndroidManifest.xml \
