fb88c1bc0b639961d409027e61929e2410d6eca5
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2013-10-04 16:35:15 +1000

    Added basic Map example. Added my API V2 key to AndroidManifest

--- src/com/example/RecordBreakers/RecordBreakers.java
+++ src/com/example/RecordBreakers/RecordBreakers.java
@@ -2,14 +2,20 @@ package com.example.RecordBreakers;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class RecordBreakers extends Activity {
     /**
      * Called when the activity is first created.
      */
+    private GoogleMap map;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        this.map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+        .getMap();
     }
 }
